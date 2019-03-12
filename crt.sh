7248eac4a3692841278e262a14d2ca0c48937a92
root <root@localhost.localdomain>
2019-03-12 03:09:34 -0400

    sort

--- crt.sh
+++ crt.sh
@@ -2,4 +2,4 @@
 
 domain=$1
 curl "https://crt.sh/?q=%25.$domain" | grep "$domain" | awk 'BEGIN{FS="<TD>"} {print $2}' | awk 'BEGIN{FS="</TD>"} {print $1}' > $domain.txt
-
+sort -u -o $domain.txt $domain.txt
