5d1e15512ba0e205b416855f43209109fc9770b3
root <root@localhost.localdomain>
2019-03-14 02:34:26 -0400

    put wildcard domains to another file for subbrute

--- crt.sh
+++ crt.sh
@@ -3,3 +3,6 @@
 domain=$1
 curl "https://crt.sh/?q=%25.$domain" | grep "$domain" | awk 'BEGIN{FS="<TD>"} {print $2}' | awk 'BEGIN{FS="</TD>"} {print $1}' > $domain.txt
 sort -u -o $domain.txt $domain.txt
+
+#remove the star at the beginning
+cat $domain.txt | grep -E '\*' | awk -F'.' -v OFS='.' '{print $2,$3}' > "$domain.to_brute.txt"
