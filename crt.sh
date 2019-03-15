580ad139c69379c22d558a5a4ea3fb0b9113a225
root <root@localhost.localdomain>
2019-03-14 23:05:25 -0400

    fix wildcard parsing bug

--- crt.sh
+++ crt.sh
@@ -5,4 +5,4 @@ curl "https://crt.sh/?q=%25.$domain" | grep "$domain" | awk 'BEGIN{FS="<TD>"} {p
 sort -u -o $domain.txt $domain.txt
 
 #remove the star at the beginning
-cat $domain.txt | grep -E '\*' | awk -F'.' -v OFS='.' '{print $2,$3}' > "$domain.to_brute.txt"
+cat $domain.txt | grep -E '\*' | sed 's/*\.//g' > "$domain.to_brute.txt"
